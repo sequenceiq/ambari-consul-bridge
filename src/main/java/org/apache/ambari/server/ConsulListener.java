25f335d483a87b3b857ef509c969d2520a40fec4
doktoric <rdoktorics@hortonworks.com>
2017-01-02 13:23:33 +0100

    fixed consulhandler

--- src/main/java/org/apache/ambari/server/ConsulListener.java
+++ src/main/java/org/apache/ambari/server/ConsulListener.java
@@ -1,14 +1,8 @@
 package org.apache.ambari.server;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.PrintWriter;
 import java.util.Date;
 
-import org.apache.ambari.server.events.AmbariEvent;
-import org.apache.ambari.server.events.ClusterEvent;
 import org.apache.ambari.server.events.ServiceComponentInstalledEvent;
-import org.apache.ambari.server.events.ServiceInstalledEvent;
 import org.apache.ambari.server.events.publishers.AmbariEventPublisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,9 +26,6 @@ public class ConsulListener {
     @Inject
     public ConsulListener(AmbariEventPublisher ambariEventPublisher) {
         LOG.info("Initialize ConsulListener for");
-        LOG.info("///////////////////////////////////");
-        LOG.info("/////INITIALIZE CONSULLISTENER/////");
-        LOG.info("///////////////////////////////////");
         ambariEventPublisher.register(this);
     }
 
@@ -44,55 +35,13 @@ public class ConsulListener {
         if(LOG.isDebugEnabled()) {
             LOG.debug(event.toString());
         }
-        LOG.info("registerServiceIntoConsul: onServiceComponentEvent.");
+        LOG.info("registerServiceIntoConsul -> ServiceComponentInstalledEvent: ", event.toString());
         registerServiceIntoConsul(event.getServiceName().toLowerCase(), event.getHostName(), null);
-
-        try(FileWriter fw = new FileWriter("/tmp/consulevents", true);
-            BufferedWriter bw = new BufferedWriter(fw); PrintWriter out = new PrintWriter(bw)) {
-            out.println(event.toString());
-        } catch (Throwable ex) {
-            LOG.info("///////////////////////////////////");
-            LOG.info("//FAILED TO REGISTER NEW SERVICE///");
-            LOG.info("///////////////////////////////////");
-            LOG.error(ex.getMessage(), ex);
-        }
-
-    }
-
-    @Subscribe
-    @AllowConcurrentEvents
-    public void onServiceEvent(ServiceInstalledEvent event) {
-        if(LOG.isDebugEnabled()) {
-            LOG.debug(event.toString());
-        }
-
-        LOG.info("registerServiceIntoConsul: registerServiceIntoConsul.");
-        registerServiceIntoConsul(event.getServiceName().toLowerCase(), "127.0.0.1", null);
-
-        try(FileWriter fw = new FileWriter("/tmp/consulevents", true);
-            BufferedWriter bw = new BufferedWriter(fw); PrintWriter out = new PrintWriter(bw)) {
-            out.println(event.toString());
-        } catch (Throwable ex) {
-            LOG.info("///////////////////////////////////");
-            LOG.info("//FAILED TO REGISTER NEW SERVICE///");
-            LOG.info("///////////////////////////////////");
-            LOG.error(ex.getMessage(), ex);
-        }
-    }
-
-    @Subscribe
-    @AllowConcurrentEvents
-    public void renameServiceEvent(ClusterEvent event) {
-        if (event.getType().equals(AmbariEvent.AmbariEventType.CLUSTER_RENAME)) {
-            registerServiceIntoConsul(null, "ip-10-0-120-236.eu-west-1.compute.internal", null);
-        } else {
-            LOG.info("Not a rename event: " + event.toString());
-        }
     }
 
     private void registerServiceIntoConsul(String serviceName, String hostName, Integer port) {
         try {
-            LOG.info("registerServiceIntoConsul: Connecting to Consul because new event arrived.");
+            LOG.info("registerServiceIntoConsul -> Connecting to Consul because new event arrived.");
             ConsulClient consulClient = createClient(CONSUL_ADDRESS, CONSUL_PORT);
             LOG.info("Successfully connected to Consul.");
             NewService newService = new NewService();
@@ -107,9 +56,6 @@ public class ConsulListener {
             consulClient.agentServiceRegister(newService);
             LOG.info("Successfully registered ne service to Consul.");
         } catch (Throwable ex) {
-            LOG.info("///////////////////////////////////");
-            LOG.info("//FAILED TO REGISTER NEW SERVICE///");
-            LOG.info("///////////////////////////////////");
             LOG.error(ex.getMessage(), ex);
         }
     }
